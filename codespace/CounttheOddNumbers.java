2fd0141242b5d3a79dfbd6c91549bbee061abd9c
Lekhan ram <lekhanram08@gmail.com>
2026-07-11 16:18:34 +0530

    Add files via upload

---
+++ codespace/CounttheOddNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class CounttheOddNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Give Inputs:");//n=8
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+            if (arr[i] % 2 != 0) {
+                count++;
+            }
+        }
+		System.out.println("The count of Odd Numbers:");
+        System.out.println(count);
+        sc.close();
+    }
+}
